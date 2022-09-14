bbbd068b79f984761635063a29b82e6e7062a9a6
fariasao <enzofarias2010@gmail.com>
2022-09-14 20:19:42 -0300

    feat: static home page with logo and games

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,68 @@
-interface ButtonProps {
-  title: string
-}
+import './styles/main.css';
 
-function Button(props: ButtonProps) {
-  return (
-    <button>
-      Enviar
-    </button>
-  )
-}
+import imgLogo from './assets/Logo.svg';
 
 function App() {
   return (
-    <>
-      <Button title="Enviar 1"/>
-      <Button title="Enviar 2"/>
-      <Button title="Enviar 3"/>
-    </>
-  )
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={imgLogo} alt="" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="bg-gradient bg-clip-text text-transparent">duo</span>{" "}
+        está aqui.
+      </h1>
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game-1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0">
+            <strong className="font-bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anuncios</span>
+          </div>
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game-2.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0">
+            <strong className="font-bold text-white block">Dota 2</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anuncios</span>
+          </div>
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game-3.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0">
+            <strong className="font-bold text-white block">Counter Strike</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anuncios</span>
+          </div>
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game-4.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0">
+            <strong className="font-bold text-white block">Apex Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anuncios</span>
+          </div>
+        </a>
+        <a href="" className='relative rounded-lg overflow-hidden'>
+          <img src="/game-5.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0">
+            <strong className="font-bold text-white block">Fortnite</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anuncios</span>
+          </div>
+        </a>
+        <a href="" className='relative'>
+          <img src="/game-6.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0">
+            <strong className="font-bold text-white block">World of Warcraft</strong>
+            <span className="text-zinc-300 text-sm block mt-1">4 anuncios</span>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App
