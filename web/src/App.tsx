ec7ca3da77aff57fb78fc6996630789452dc16ee
fariasao <enzofarias2010@gmail.com>
2022-10-18 21:56:17 -0300

    feat: the init of modal and stylization

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,6 +7,7 @@ import { GameController } from 'phosphor-react';
 import './styles/main.css';
 
 import imgLogo from './assets/Logo.svg';
+import { Input } from './components/form/input';
 
 interface Game {
   id: string;
@@ -14,20 +15,19 @@ interface Game {
   bannerUrl: string;
   _count: {
     ads: number;
-  }
+  };
 }
 
 function App() {
   const [games, setGames] = useState<Game[]>([]);
 
-  useEffect(() =>{
-    fetch('http://localhost:3333/games')
-      .then(response => response.json())
-      .then(data => {
+  useEffect(() => {
+    fetch("http://localhost:3333/games")
+      .then((response) => response.json())
+      .then((data) => {
         setGames(data);
-      })
-
-  }, [])
+      });
+  }, []);
 
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
@@ -38,17 +38,16 @@ function App() {
         está aqui.
       </h1>
       <div className="grid grid-cols-6 gap-6 mt-16">
-        {games.map(game => {
+        {games.map((game) => {
           return (
             <GameBanner
               key={game.id}
-              bannerUrl={game.bannerUrl} 
-              title={game.title }
+              bannerUrl={game.bannerUrl}
+              title={game.title}
               adsCount={game._count.ads}
             />
-          )
+          );
         })}
-   
       </div>
       <Dialog.Root>
         <CreateAdBanner />
@@ -56,65 +55,88 @@ function App() {
         <Dialog.Portal>
           <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
 
-          <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg overflow-hidden w-[480px]" >
-            <Dialog.Title className="text-3xl text-white font-black" >Publique um Anuncio</Dialog.Title>
+          <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg overflow-hidden w-[480px]">
+            <Dialog.Title className="text-3xl text-white font-black">
+              Publique um Anuncio
+            </Dialog.Title>
 
-            <Dialog.Content>
-              <form>
-                <div>
-                  <label htmlFor="game">Qual o game?</label>
-                  <input id="game" placeholder="Selecione o game que deseja jogar" />
+            <form className="mt-8 flex flex-col gap-4">
+              <div className="flex flex-col gap-2">
+                <label className="font-semibold" htmlFor="game">
+                  Qual o game?
+                </label>
+                <Input
+                  id="game"
+                  placeholder="Selecione o game que deseja jogar"
+                />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label htmlFor="name">Seu nome (ou nickname)</label>
+                <Input id="name" placeholder="Como te chamam dentro do game?" />
+              </div>
+
+              <div className="grid grid-cols-2 gap-6">
+                <div className="flex flex-col gap-2">
+                  <label htmlFor="yearsPlaying">Joga ha quantos anos?</label>
+                  <Input
+                    id="yearsplaying "
+                    type="number"
+                    placeholder="Tudo bem ser Zero"
+                  />
                 </div>
-
-                <div>
-                  <label htmlFor="name">Seu nome (ou nickname)</label>
-                  <input id="name" placeholder="Como te chamam dentro do game?"/>
+                <div className="flex flex-col gap-2">
+                  <label htmlFor="discord">Qual seu Discord?</label>
+                  <Input id="discord" placeholder="Usuario#0000" />
                 </div>
+              </div>
 
-                <div>
-                  <div>
-                    <label htmlFor="yearsPlaying">Joga ha quantos anos?</label>
-                    <input id="yearsplaying "type="number" placeholder="Tudo bem ser Zero" />
-                  </div>
-                  <div>
-                    <label htmlFor="discord">Qual seu Discord?</label>
-                    <input id="discord" placeholder="Usuario#0000" />
-                  </div>
-                </div>
+              <div className="flex flex-col gap-6">
+                <div className="flex flex-col gap-2">
+                  <label htmlFor="weekDays">Quando costuma jogar?</label>
 
-                <div>
                   <div>
-                    <label htmlFor="weekDays">Quando costuma jogar?</label>
-                  </div>
-                  <div>
-                    <label htmlFor="hourStart">Qual horario do dia?</label>
-                  </div>
-                  <div>
-                    <input id="hourStart" type="time" placeholder="De" />
-                    <input id="hourEnd" type="time" placeholder="ate" />
+                    <button>S</button>
+                    <button>T</button>
+                    <button>Q</button>
+                    <button>Q</button>
+                    <button>S</button>
+                    <button>S</button>
+                    <button>D</button>
                   </div>
                 </div>
-
-                <div>
-                  <input type="checkbox" />
-                  Costumo me conectar ao chat de voz
+                <div className="flex flex-col gap-2 flex-1">
+                  <label htmlFor="hourStart">Qual horario do dia?</label>
                 </div>
+                <div className="grid grid-cols-2 gap-2">
+                  <Input id="hourStart" type="time" placeholder="De" />
+                  <Input id="hourEnd" type="time" placeholder="ate" />
+                </div>
+              </div>
 
-                <footer>
-                  <button>Cancelar</button>
-                  <button type="submit">
-                    <GameController />
-                    Encontrar duo
-                  </button>
-                </footer>
-              </form>
-            </Dialog.Content>
+              <div className="mt-2 flex gap-2 text-sm">
+                <Input type="checkbox" />
+                Costumo me conectar ao chat de voz
+              </div>
+
+              <footer className="mt-4 flex justify-end gap-4">
+                <Dialog.Close className="bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600">
+                  Cancelar
+                </Dialog.Close>
+                <button
+                  className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600"
+                  type="submit"
+                >
+                  <GameController className="w-6 h-6" />
+                  Encontrar duo
+                </button>
+              </footer>
+            </form>
           </Dialog.Content>
         </Dialog.Portal>
-
       </Dialog.Root>
     </div>
   );
 }
 
-export default App
+export default App;
