956e05116cae17bf6ae681921436327fdbc85c8f
fariasao <enzofarias2010@gmail.com>
2022-10-19 21:37:57 -0300

    build: connect backend to mobile app

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -6,9 +6,11 @@ import { styles } from "./styles";
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType
+  title: string;
+  _count: {
+    ads: number;
+  };
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -19,13 +21,13 @@ interface Props extends TouchableOpacityProps {
 export function GameCard({ data, ...rest }:Props) {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
-      <ImageBackground style={styles.cover} source={data.cover}>
+      <ImageBackground style={styles.cover} source={{ uri: data.bannerUrl}}>
         <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
           <Text style={styles.name}>
-            {data.name}
+            {data.title}
           </Text>
           <Text style={styles.ads}>
-            {data.ads} anuncios
+            {data._count.ads} anuncios
           </Text>
         </LinearGradient>
       </ImageBackground>
