a4fb3bf6d33249c81e95823401c1c7d40ab5e01f
fariasao <enzofarias2010@gmail.com>
2022-10-20 21:31:54 -0300

    feat: add types of params games

--- mobile/src/@types/@navigation.d.ts
+++ mobile/src/@types/@navigation.d.ts
@@ -1,8 +1,15 @@
+export interface GameParams {
+    id: string;
+    title: string;
+    bannerUrl: string;
+}
+
+
 export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       home: undefined;
-      game: undefined;
+      game: GameParams;
     }
   }
 }
--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,10 +1,19 @@
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useRoute } from '@react-navigation/native'
 
 import { Background } from '../../components/Background';
 
 import { styles } from './style';
+import { GameParams } from '../../@types/@navigation';
+
 
 export function Game() {
+
+  const route = useRoute()
+  const game = route.params as GameParams;
+  // console.log(game);
+  
+
   return (
     <Background>
       <SafeAreaView style={styles.container}></SafeAreaView>
